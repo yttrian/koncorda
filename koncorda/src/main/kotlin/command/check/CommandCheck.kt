c931d0d1dd03822f5ffef2b4e7d3d44724071cd8
Ian Moore <privet.privet250@gmail.com>
2021-07-03 16:28:53 -0400

    Allow combining CommandChecks and using Message and Embed with respond

--- koncorda/src/main/kotlin/command/check/CommandCheck.kt
+++ koncorda/src/main/kotlin/command/check/CommandCheck.kt
@@ -5,3 +5,23 @@ import org.yttr.koncorda.command.CommandCall
 interface CommandCheck {
     suspend fun check(call: CommandCall): Boolean
 }
+
+infix fun CommandCheck.or(right: CommandCheck): CommandCheck = composite(this, right, Boolean::or)
+
+infix fun CommandCheck.and(right: CommandCheck): CommandCheck = composite(this, right, Boolean::and)
+
+operator fun CommandCheck.not(): CommandCheck = object : CommandCheck {
+    override suspend fun check(call: CommandCall): Boolean = !this@not.check(call)
+}
+
+private fun composite(
+    left: CommandCheck,
+    right: CommandCheck,
+    predicate: (Boolean, Boolean) -> Boolean
+): CommandCheck {
+    return object : CommandCheck {
+        override suspend fun check(call: CommandCall): Boolean {
+            return predicate(left.check(call), right.check(call))
+        }
+    }
+}
